02126dc1a73814b0b546c7089b6ddfd99d3584db
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-07-08 12:26:42 +0530

    recoil added

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+
+import { useBalance } from "@repo/store/useBalance"
+
+// export default function () {
+//   const balance = useBalance();
+//   return <div>
+//     hi there {balance}
+//   </div>
+// }
+
+export default function Home() {
+  const balance = useBalance();
+  return <div>
+    hi there {balance}
+  </div>
+}

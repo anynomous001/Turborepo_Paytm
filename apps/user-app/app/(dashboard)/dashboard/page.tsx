224a97b4bb64e70911ed4345db571008a30ecd90
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-08-06 20:52:21 +0530

    added space to user-app

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,9 @@ import Center from "@repo/ui/Center"
 import AddMoneyCard from "../../components/AddMoneyCard"
 import { BalanceCard } from "../../components/BalanceCard"
 
+
+
+
 const page = () => {
     return (
         <div className="flex  pt-20 justify-around gap-6  min-h-full min-w-full ">

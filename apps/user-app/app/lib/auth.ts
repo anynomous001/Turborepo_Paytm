596b28feafeb7451dd55f2375965e7e6c184ba91
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-11-07 13:55:56 +0530

    dashboard branch build and ready to march

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -5,7 +5,7 @@ import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as any,
     session: {
         strategy: "jwt",
     },
